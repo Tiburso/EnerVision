4d0aa202105fe75f181e0f82eebef0fd810d2b30
Manuel <manuelmariamtc@gmail.com>
2024-03-30 23:45:28 +0100

    Update loss function and optimizer settings

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import torch
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 
-from torchmetrics.functional.classification import dice
-from torchvision.ops.focal_loss import sigmoid_focal_loss
+from unified_focal_loss import AsymmetricUnifiedFocalLoss
 
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
@@ -18,27 +17,6 @@ from models.architectures import (
 
 from dataloaders.solar_dk_dataset import SolarDKDataset
 import torchvision.transforms.v2 as transforms
-
-
-class CombinedLoss(torch.nn.Module):
-    """
-    Combined loss class combines both the binary cross entropy loss and the dice loss
-    """
-
-    def __init__(self, alpha=0.5, treshold=0.5):
-        super(CombinedLoss, self).__init__()
-        self.alpha = alpha
-        self.treshold = treshold
-
-    def forward(self, y_pred, y_true):
-        focal_loss = sigmoid_focal_loss(y_pred, y_true, reduction="mean")
-        dice_loss = dice(y_pred, y_true.int(), threshold=self.treshold)
-
-        # Normalize the dice loss
-        dice_loss = 1 - dice_loss
-
-        return self.alpha * focal_loss + (1 - self.alpha) * dice_loss
-
 
 train_folder = "data/solardk_dataset_neurips_v2/gentofte_trainval/train"
 validation_folder = "data/solardk_dataset_neurips_v2/gentofte_trainval/val"
@@ -63,9 +41,9 @@ train_dataset = SolarDKDataset(train_folder, transform=train_transform)
 validation_dataset = SolarDKDataset(validation_folder, transform=test_transform)
 test_dataset = SolarDKDataset(test_folder, transform=test_transform)
 
-train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=4)
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=4)
 validation_loader = DataLoader(
-    validation_dataset, batch_size=8, shuffle=False, num_workers=4
+    validation_dataset, batch_size=16, shuffle=False, num_workers=4
 )
 test_loader = DataLoader(test_dataset, batch_size=4, shuffle=False, num_workers=4)
 
@@ -75,8 +53,8 @@ model = DeepLabModel(num_classes=1, backbone="resnet50")
 # model = Yolov8Model(num_classes=1)
 
 treshold = 0.1
-loss_fn = CombinedLoss(alpha=0.5, treshold=treshold)
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+loss_fn = AsymmetricUnifiedFocalLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
 scheduler = ReduceLROnPlateau(optimizer, mode="min", factor=0.1, patience=10)
 
 base_model = BaseModel(model, loss_fn, optimizer, scheduler, treshold)
