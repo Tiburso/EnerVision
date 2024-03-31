db8c0649aaaa781639a2398dbd3ba6f458d6ff26
Manuel <manuelmariamtc@gmail.com>
2024-03-31 19:33:46 +0200

    Use the full dataset images again, change loss and scheduler for paper ones

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # From the dataset, create a train and test set
 import torch
+from torch import nn
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 
 from losses import AsymmetricUnifiedFocalLoss
+
+from segmentation_models_pytorch.losses import DiceLoss, JaccardLoss
 from torchmetrics.functional.classification import dice
 
-from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.optim.lr_scheduler import PolynomialLR
 
 # Add EarlyStopping
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
@@ -14,26 +17,25 @@ from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 from models.base import BaseModel
 from models.architectures import (
     DeepLabModel,
-    FCNResNetModel,
-    MaskRCNNModel,
-    Yolov8Model,
 )
 
 from dataloaders.solar_dk_dataset import SolarDKDataset
 import torchvision.transforms.v2 as transforms
 
 
-class CombinedBCEDiceLoss(torch.nn.Module):
-    def __init__(self, alpha=0.5):
-        super(CombinedBCEDiceLoss, self).__init__()
-        self.bce = torch.nn.BCEWithLogitsLoss()
-        self.alpha = alpha
+class CombinedLoss(nn.Module):
+    def __init__(self):
+        super(CombinedLoss, self).__init__()
+        self.cross_entropy = nn.CrossEntropyLoss()
+        self.dice_loss = DiceLoss(mode="multiclass")
+        self.jaccard_loss = JaccardLoss(mode="multiclass")
 
-    def forward(self, y_hat, y):
-        bce = self.bce(y_hat, y)
-        y_hat = torch.sigmoid(y_hat)
-        dice_loss = 1 - dice(y_hat, y.int())
-        return self.alpha * bce + (1 - self.alpha) * dice_loss
+    def forward(self, y_pred, y_true):
+        cross_entropy_loss = self.cross_entropy(y_pred, y_true)
+        dice_loss = self.dice_loss(y_pred, y_true)
+        jaccard_loss = self.jaccard_loss(y_pred, y_true)
+
+        return cross_entropy_loss + 2 * dice_loss + 3 * jaccard_loss
 
 
 train_folder = "data/solardk_dataset_neurips_v2/gentofte_trainval/train"
@@ -62,11 +64,12 @@ test_loader = DataLoader(test_dataset, batch_size=4, shuffle=False, num_workers=
 
 model = DeepLabModel(num_classes=2, backbone="resnet152")
 
-treshold = 0.5
-loss_fn = AsymmetricUnifiedFocalLoss(weight=0.4, delta=0.9, gamma=0.3)
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-4)
+loss_fn = CombinedLoss()
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-2, weight_decay=1e-3)
+scheduler = PolynomialLR(optimizer, power=0.9, max_epochs=100)
 
-base_model = BaseModel(model, loss_fn, optimizer, treshold=treshold)
+base_model = BaseModel(model, loss_fn, optimizer, scheduler=scheduler)
+
 trainer = pl.Trainer(
     max_epochs=150,
     min_epochs=5,
