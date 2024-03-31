0eec1fa2e98d3cb6dce553b2bf9d30666fc7516b
Manuel <manuelmariamtc@gmail.com>
2024-03-31 14:13:43 +0200

    Update batch sizes and backbone model in main.py

--- main.py
+++ main.py
@@ -59,13 +59,13 @@ train_dataset = SolarDKDataset(train_folder, transform=train_transform)
 validation_dataset = SolarDKDataset(validation_folder, transform=test_transform)
 test_dataset = SolarDKDataset(test_folder, transform=test_transform)
 
-train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=4)
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4)
 validation_loader = DataLoader(
-    validation_dataset, batch_size=8, shuffle=False, num_workers=4
+    validation_dataset, batch_size=32, shuffle=False, num_workers=4
 )
 test_loader = DataLoader(test_dataset, batch_size=4, shuffle=False, num_workers=4)
 
-model = DeepLabModel(num_classes=2, backbone="resnet159")
+model = DeepLabModel(num_classes=2, backbone="resnet101")
 
 treshold = 0.5
 loss_fn = AsymmetricUnifiedFocalLoss(delta=0.80)
