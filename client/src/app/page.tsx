8036392f60503e69c8f1bb96fd2b32a027f60e4c
Tiburso <manuelmariamc@gmail.com>
2024-05-05 17:45:08 +0200

    Start with components library and next

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,21 +1,19 @@
 "use client";
 
 import {
-  useJsApiLoader,
-  GoogleMap,
-  MarkerF,
-  CircleF,
+    useJsApiLoader,
+    GoogleMap,
 } from '@react-google-maps/api';
 
-import { useMemo, useState } from 'react';
+import React from 'react';
+import { Button } from '@/components/ui/button';
 
-import styles from './page.module.css';
+import { useMemo, useState } from 'react';
 
 export default function Home() {
   const [lat, setLat] = useState(27.672932021393862);
   const [lng, setLng] = useState(85.31184012689732);
 
-  const libraries = useMemo(() => ['places'], []);
   const mapCenter = useMemo(() => ({ lat: lat, lng: lng }), [lat, lng]);
 
   const mapOptions = useMemo<google.maps.MapOptions>(
@@ -23,53 +21,39 @@ export default function Home() {
         disableDefaultUI: true,
         clickableIcons: true,
         scrollwheel: false,
+        zoomControl: false,
         }),
       []
   );
 
   const { isLoaded } = useJsApiLoader({
-      id: 'google-map-script',
-      googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
+      googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,  
   });
-
-  console.log(process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY);
-
+  
   if (!isLoaded) {
       return <p>Loading...</p>;
   }
 
   return (
-      <div className={styles.homeWrapper}>
-      
+      <div>
           <GoogleMap
-              options={mapOptions}
-              zoom={20}
-              center={mapCenter}
               mapTypeId={google.maps.MapTypeId.SATELLITE}
-              mapContainerStyle={{ width: '800px', height: '800px' }}
+              options={mapOptions}
+              center={mapCenter}
+              zoom={20}
+              mapContainerStyle={{ width: '800px', height: window.innerHeight }}
               onLoad={(map) => console.log('Map Loaded')}
           >
-              <MarkerF
-                  position={mapCenter}
-                  onLoad={() => console.log('Marker Loaded')}
-              />
-
-              {/* {[1000, 2500].map((radius, idx) => {
-                  return (
-                      <CircleF
-                      key={idx}
-                      center={mapCenter}
-                      radius={radius}
-                      onLoad={() => console.log('Circle Load...')}
-                          options={{
-                              fillColor: radius > 1000 ? 'red' : 'green',
-                              strokeColor: radius > 1000 ? 'red' : 'green',
-                              strokeOpacity: 0.8,
-                          }}
-                      />
-                  );
-              })} */}
           </GoogleMap>
+
+          <Button
+            variant='secondary'
+            onClick={() => {
+                console.log('Button clicked');
+            }}
+          >
+            Click me
+          </Button>
       </div>
   );
 };
