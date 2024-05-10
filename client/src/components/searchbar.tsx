41f2e87ef7fd8182b05754e298a549944ef727f5
Tiburso <manuelmariamc@gmail.com>
2024-05-10 21:17:26 +0200

    Add places autocomplete to the UI

--- client/src/components/searchbar.tsx
+++ client/src/components/searchbar.tsx
@@ -1,12 +1,29 @@
 import { SearchIcon } from "lucide-react"
 import { Input } from "@/components/ui/input"
 
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from 'use-places-autocomplete';
+
 export interface SearchbarProps {
   className?: string
   onClick?: () => void
 }
 
 export function Searchbar({className, onClick} : SearchbarProps) {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: { componentRestrictions: { country: 'nl' } },
+    debounce: 300,
+    cache: 86400,
+  });
+
   return (
     <div className={className}>
       <SearchIcon className="text-gray-500 translate-y-8 ml-1" />
@@ -14,18 +31,34 @@ export function Searchbar({className, onClick} : SearchbarProps) {
         className="w-full pl-8 rounded bg-w text-gray-800 focus-visible:ring-0 focus-visible:outline-none"
         placeholder="Search location..."
         type="search"
+        disabled={!ready}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
       />
       
-      <ul className="text-sm text-gray-700 bg-white rounded mt-1">
-        {[1, 2, 3].map((option, index) => (
-          <li key={index}>
-          <a onClick={onClick} className="block rounded px-4 py-2 hover:bg-gray-200 cursor-pointer">
-            Recent Search {option}
-          </a>
-        </li>
-        ))}
+      {status === 'OK' && (
+        <ul className="text-sm text-gray-700 bg-white rounded mt-1">
+        {data.map((suggestion) => {
+          const {
+            place_id,
+            structured_formatting: { main_text, secondary_text },
+            description,
+          } = suggestion;
+
+          return (
+            <li key={place_id}>
+            <a onClick={() => {
+              setValue(description, false);
+              clearSuggestions();
+              onClick && onClick();
+            }} className="block rounded px-4 py-2 hover:bg-gray-200 cursor-pointer">
+              <strong>{main_text}</strong> <small>{secondary_text}</small>
+            </a>
+          </li>
+         )}
+        )}
       </ul>
-      
+      )}
     </div>
   )
 }
